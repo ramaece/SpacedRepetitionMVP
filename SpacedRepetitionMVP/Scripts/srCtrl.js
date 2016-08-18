9b92ba660498e6c659421f46a8eaa9ce44e58211
ramaece <ramaece@yahoo.com>
2016-08-18 23:06:28 +0530

    30 Questions Integrated

--- SpacedRepetitionMVP/Scripts/srCtrl.js
+++ SpacedRepetitionMVP/Scripts/srCtrl.js
@@ -40,6 +40,228 @@ app.controller('srCtrl', function ($scope) {
     C3: 'are pre-historic burial monuments',
     C4: 'are monoliths belonging to the Neolithic period',
     correctAnswer: 'are monoliths belonging to the Neolithic period'
+},
+{
+    ques: 'From which one of the following regions the earliest evidence of the rice cultivation has come?',
+    C1: 'Belan Valley',
+    C2: 'Central Ganga Valley',
+    C3: 'Gomal  Valley',
+    C4: 'Bolan Valley',
+    correctAnswer: 'Belan Valley'
+},
+
+{
+    ques: '"Sohan Culture" is another name of ?',
+    C1: 'Paleolithic Culture',
+    C2: 'Mesolithic Culture',
+    C3: 'Neolithic Culture',
+    C4: 'Chalcolithic Culture',
+    correctAnswer: 'Paleolithic Culture'
+},
+
+{
+    ques: 'Which one of the following sites is the largest Mesolithic site in India?',
+    C1: 'Bagor',
+    C2: 'Tilwara',
+    C3: 'Valasana',
+    C4: 'Akhaj',
+    correctAnswer: 'Bagor'
+},
+
+{
+    ques: 'Which one of the following Mesolithic sites provides evidence of a battle?',
+    C1: 'Bagor',
+    C2: 'Sarai Nahar Rai',
+    C3: 'Langhanaj',
+    C4: 'Mahadaha',
+    correctAnswer: 'Sarai Nahar Rai'
+},
+{
+    ques: 'Which one of the following Mesolithic sites has the distinction of being the first site to be discovered in the arid zone?',
+    C1: 'Langhanaj',
+    C2: 'Bagor',
+    C3: 'Sidhpur',
+    C4: 'Barasimla',
+    correctAnswer: 'Langhanaj'
+},
+{
+    ques: 'Which animal figures most in the cave paintings of Mesolithic age?',
+    C1: 'Buffalo',
+    C2: 'Deer',
+    C3: 'Tiger',
+    C4: 'Chinkara',
+    correctAnswer: 'Deer'
+},
+
+{
+    ques: 'From which one of the following Mesolithic sites charred and corbonised grains of wild rice embedded in the burnt clay remains of wattle and daub walls of huts, has been discovered?',
+    C1: 'Chopani Mando',
+    C2: 'Mahadaha',
+    C3: 'Barkaccha',
+    C4: 'Langhanaj',
+    correctAnswer: 'Chopani Mando'
+},
+
+{
+    ques: 'Which one of the following statements is not true about the Chalcolithic cultures of India?',
+    C1: 'They were primarily communities in the hilly and riverine areas',
+    C2: 'They mostly used stone and copper objects and occasionally low grade bronze',
+    C3: 'A large number of them appeared after the end of the bronze Harappan Culture',
+    C4: 'They were restricted to the Ganga-Yamuna doab and foothills of the Himalayas',
+    correctAnswer: 'They were restricted to the Ganga-Yamuna doab and foothills of the Himalayas'
+},
+
+{
+    ques: 'Which one of the following pairs is/are correctly matched?    ',
+    C1: 'Harappan Civilization : Painted Grey Ware',
+    C2: 'The Kushans : Gandhara School of Art',
+    C3: 'The Mughals : Ajanta Paintings',
+    C4: 'The Marathas : Pahari School of Painting',
+    correctAnswer: 'The Kushans : Gandhara School of Art'
+},
+
+{
+    ques: 'At which one of the following Neolithic sites, the evidence of the unique practice of burying the dogs with their masters in the caves has been found?',
+    C1: 'Burzahom',
+    C2: 'Piklihal',
+    C3: 'Gufkral',
+    C4: 'Chirand',
+    correctAnswer: 'Burzahom'
+},
+
+{
+    ques: 'Which One of the following cultures is influenced by Harappan culture?',
+    C1: 'Malwa Culture',
+    C2: 'Jorwe Culture',
+    C3: 'Kayatha Culture',
+    C4: 'Megalithic Culture',
+    correctAnswer: 'Kayatha Culture'
+},
+
+{
+    ques: 'At which one of the following Mesolithic sites thatched wind screens were constructed?',
+    C1: 'Adamgarh',
+    C2: 'Langhanaj',
+    C3: 'Birbhanpur',
+    C4: 'Bagor',
+    correctAnswer: 'Bagor'
+},
+
+{
+    ques: 'Which is one of the most extensively excavated Chalcolithic age rural sitesin the continent?',
+    C1: 'Daimabad',
+    C2: 'Navdatoli',
+    C3: 'kayatha',
+    C4: 'Nindowari',
+    correctAnswer: 'Navdatoli'
+},
+
+{
+    ques: 'Which one of the following statements regarding Harappan Civilisation is correct?',
+    C1: 'The standard Harappan seals were made of clay',
+    C2: 'The inhabitants of Harappa had neither knowledge of copper nor bronze',
+    C3: 'The Harappan Civilisation was rural based',
+    C4: 'The inhabitants of Harappa grew and used cotton',
+    correctAnswer: 'The inhabitants of Harappa grew and used cotton'
+},
+
+{
+    ques: 'Three most common features of all Indus Valley site were',
+    C1: 'climate,  flora-fauna and artifical irrigation',
+    C2: 'desert, rivers and zoological features ',
+    C3: 'use of baked bricks and pottery, elaborate drainage system and occurrence of marsh or jungle animals',
+    C4: 'building, town planning and burial system',
+    correctAnswer: 'use of baked bricks and pottery, elaborate drainage system and occurrence of marsh or jungle animals'
+},
+
+{
+    ques: 'The most important item of import in the Harappan civilisation were',
+    C1: 'metals and precious stones',
+    C2: 'food grains',
+    C3: 'textiles',
+    C4: 'pottery',
+    correctAnswer: 'metals and precious stones'
+},
+
+{
+    ques: 'A piece of woven cotton has been recoveredfrom which one of the following Harappan sites?',
+    C1: 'Harappa',
+    C2: 'Lothal',
+    C3: 'Banawali',
+    C4: 'Mohenjodaro',
+    correctAnswer: 'Mohenjodaro'
+},
+
+{
+    ques: 'Which animal is engraved on most of the Harappan seals?',
+    C1: 'Humpless bull or Unicorn',
+    C2: 'Elephant',
+    C3: 'Bison',
+    C4: 'Tiger',
+    correctAnswer: 'Humpless bull or Unicorn'
+},
+
+{
+    ques: 'Rows of distinctive five altars with the provision of ritual bathing have been found at',
+    C1: 'Mohenjodaro',
+    C2: 'Harappa',
+    C3: 'Kalibangan',
+    C4: 'Lothal',
+    correctAnswer: 'Kalibangan'
+},
+
+{
+    ques: 'The Harappan towns and cities were divided in…… large-blocks.',
+    C1: 'square',
+    C2: 'rectangular',
+    C3: 'circular',
+    C4: 'semicircular',
+    correctAnswer: 'rectangular'
+},
+
+{
+    ques: 'Which of the following sites excavated recently shows all the three stages of Harappan Occupations (Pre-Harappan, Harappan and Post Harappan)?',
+    C1: 'Rojdi',
+    C2: 'Surkotda',
+    C3: 'Desalpur',
+    C4: 'All of these',
+    correctAnswer: 'All of these'
+},
+
+{
+    ques: 'Which one of the following was not cultivated in the Harappan Civilisation?',
+    C1: 'Mustard',
+    C2: 'Barley',
+    C3: 'Sugarcane',
+    C4: 'Sesamum',
+    correctAnswer: 'Sugarcane'
+},
+
+{
+    ques: 'Harappan weapons were made of ',
+    C1: 'stone',
+    C2: 'copper',
+    C3: 'bronze',
+    C4: 'All of the above',
+    correctAnswer: 'All of the above'
+},
+
+{
+    ques: 'The detification/worship of which of the following was not a part of the religion of Indus Valley People?',
+    C1: 'Mother Goddess',
+    C2: 'Forces of Nature',
+    C3: 'Trees and their spirits',
+    C4: 'Certain animal chimeras and their anthropic figures ',
+    correctAnswer: 'Forces of Nature'
+},
+
+{
+    ques: 'Which one among the following is not the characteristic feature of the Harappan settlement?',
+    C1: 'Doorways and windows generally faced the side lanes and rarely opened onto the main streets',
+    C2: 'Houses generally had separate bathing areas and toilets',
+    C3: 'The citadel was walled but the lower town was not walled',
+    C4: 'Drains and water chutes from the second storey were often built inside the wall',
+    correctAnswer: 'Drains and water chutes from the second storey were often built inside the wall'
 }
 
     ];
